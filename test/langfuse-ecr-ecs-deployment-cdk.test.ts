6525193af0eacd42160a9cbaaf201b864628981c
KennyDizi <xamarindevelopervietnam@outlook.com>
2024-01-01 21:05:14 +0700

    Init project

---
+++ test/langfuse-ecr-ecs-deployment-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LangfuseEcrEcsDeploymentCdk from '../lib/langfuse-ecr-ecs-deployment-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/langfuse-ecr-ecs-deployment-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LangfuseEcrEcsDeploymentCdk.LangfuseEcrEcsDeploymentCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
