5ec786e87847cd4b62897f7942b3eab27d1c4624
KennyDizi <xamarindevelopervietnam@outlook.com>
2024-01-06 11:10:22 +0700

    Add document for LangfuseDockerImageEcsDeploymentCdkStackProps

--- lib/LangfuseDockerImageEcsDeploymentCdkStackProps.ts
+++ lib/LangfuseDockerImageEcsDeploymentCdkStackProps.ts
@@ -1,7 +1,12 @@
+import * as cdk from 'aws-cdk-lib';
 import { DatabaseEnvTyped, DockerEnvTyped } from '../process-env-typed';
 import { LangfuseDockerImageEcrDeploymentCdkStackProps } from './LangfuseDockerImageEcrDeploymentCdkStackProps';
 
 export interface LangfuseDockerImageEcsDeploymentCdkStackProps extends LangfuseDockerImageEcrDeploymentCdkStackProps {
+    /**
+     * The Docker image version to be deployed.
+     */
+    readonly imageVersion: string;
     /**
      * The port number on which the container service will be available.
      */
@@ -14,7 +19,13 @@ export interface LangfuseDockerImageEcsDeploymentCdkStackProps extends LangfuseD
      * The Docker environment variables and run arguments.
      */
     readonly dockerRunArgs: DockerEnvTyped,
-
-
+    /**
+     * The database environment variables and arguments.
+     */
     readonly databaseArgs: DatabaseEnvTyped;
+    /**
+     * The ECR repository where the Docker images will be stored.
+     */
+
+    readonly ecrRepository: cdk.aws_ecr.Repository
 }
