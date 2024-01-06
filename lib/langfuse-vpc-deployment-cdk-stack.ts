3c8afdc78879c938dc3b60f0c7ee7b64840d571a
KennyDizi <xamarindevelopervietnam@outlook.com>
2024-01-06 11:43:35 +0700

    Add LangfuseVpcDeploymentCdkStack

--- lib/langfuse-vpc-deployment-cdk-stack.ts
+++ lib/langfuse-vpc-deployment-cdk-stack.ts
@@ -2,9 +2,24 @@ import * as cdk from "aws-cdk-lib";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
 import * as iam from "aws-cdk-lib/aws-iam";
 import * as logs from "aws-cdk-lib/aws-logs";
+import { Construct } from 'constructs';
 import { FlowLogTrafficType } from "aws-cdk-lib/aws-ec2";
 import { RetentionDays } from "aws-cdk-lib/aws-logs";
 import { LangfuseDockerImageEcsDeploymentCdkStackProps } from "./LangfuseDockerImageEcsDeploymentCdkStackProps";
+
+/**
+ * Represents a CDK stack for deploying a VPC suitable for ECS deployments.
+ * This stack creates a VPC with public and private subnets, NAT gateways,
+ * and VPC flow logs for monitoring network traffic.
+ */
+export class LangfuseVpcDeploymentCdkStack extends cdk.NestedStack {
+    public readonly vpc: ec2.Vpc;
+
+    constructor(scope: Construct, id: string, props: LangfuseDockerImageEcsDeploymentCdkStackProps) {
+        super(scope, id, props);
+        this.vpc = createVPC(this, props);
+    }
+}
 
 /**
  * Creates a VPC with public and private subnets, NAT gateways, and VPC flow logs.
