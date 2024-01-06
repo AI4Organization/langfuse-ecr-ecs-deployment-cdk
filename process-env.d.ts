69837f1d39cb2ecfe852c0761e6a2b189f5681b7
KennyDizi <xamarindevelopervietnam@outlook.com>
2024-01-06 13:09:59 +0700

    Update document for environment variables

--- process-env.d.ts
+++ process-env.d.ts
@@ -30,11 +30,52 @@ declare module NodeJS {
          */
         PORT: string;
         /**
-         * Port number of the databse will run.
+         * Port number on which the database server is listening.
          */
         DB_PORT: string;
+        /**
+         * Username for the database connection.
+         */
         POSTGRES_USER: string;
+        /**
+         * Password for the database connection.
+         */
         POSTGRES_PASSWORD: string;
+        /**
+         * Name of the database to connect to.
+         */
         POSTGRES_DB: string;
+        /**
+         * The environment the application is running in.
+         */
+        NODE_ENV: string;
+        /**
+         * The connection string for the application's database.
+         */
+        DATABASE_URL: string;
+        /**
+         * The secret used by NextAuth for authentication.
+         */
+        NEXTAUTH_SECRET: string;
+        /**
+         * A salt string for hashing or encryption.
+         */
+        SALT: string;
+        /**
+         * The base URL for the NextAuth application.
+         */
+        NEXTAUTH_URL: string;
+        /**
+         * Flag to enable or disable telemetry.
+         */
+        TELEMETRY_ENABLED: string;
+        /**
+         * Flag to enable or disable sign-up.
+         */
+        NEXT_PUBLIC_SIGN_UP_DISABLED: string;
+        /**
+         * Flag to enable or disable experimental features.
+         */
+        LANGFUSE_ENABLE_EXPERIMENTAL_FEATURES: string;
     }
 }
