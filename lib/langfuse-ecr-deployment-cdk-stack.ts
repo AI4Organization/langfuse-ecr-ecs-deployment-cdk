30290a33aab235e5cf3bfcd5321f7b82cb76f574
KennyDizi <xamarindevelopervietnam@outlook.com>
2024-01-06 11:00:50 +0700

    update document for LangfuseEcrEcsDeploymentCdkStack

--- lib/langfuse-ecr-deployment-cdk-stack.ts
+++ lib/langfuse-ecr-deployment-cdk-stack.ts
@@ -5,38 +5,56 @@ import * as ecr from 'aws-cdk-lib/aws-ecr';
 import { LangfuseDockerImageEcrDeploymentCdkStackProps } from './LangfuseDockerImageEcrDeploymentCdkStackProps';
 import { LATEST_IMAGE_VERSION } from '../bin/langfuse-ecr-ecs-deployment-cdk';
 
-export class LangfuseEcrEcsDeploymentCdkStack extends cdk.Stack {
+/**
+ * Represents a CDK stack for deploying Docker images to an Amazon ECR repository.
+ * This stack creates an ECR repository, sets up lifecycle rules for image retention,
+ * and handles the deployment of specified image versions from a Docker registry to ECR.
+ */
+export class LangfuseEcrEcsDeploymentCdkStack extends cdk.NestedStack {
+  /**
+   * The ECR repository where Docker images are stored.
+   * @public
+   * @type {cdk.aws_ecr.Repository}
+   */
+  public readonly ecrRepository: cdk.aws_ecr.Repository;
+
+  /**
+   * Constructs a new instance of the LangfuseEcrEcsDeploymentCdkStack class.
+   * @param {Construct} scope - The scope in which to define this construct.
+   * @param {string} id - The scoped construct ID. Must be unique amongst siblings in the same scope.
+   * @param {LangfuseDockerImageEcrDeploymentCdkStackProps} props - The stack properties.
+   */
   constructor(scope: Construct, id: string, props: LangfuseDockerImageEcrDeploymentCdkStackProps) {
     super(scope, id, props);
 
-    const ecrRepository = new ecr.Repository(this, `${props.appName}-${props.environment}-DockerImageEcrRepository`, {
+    this.ecrRepository = new ecr.Repository(this, `${props.appName}-${props.environment}-DockerImageEcrRepository`, {
       repositoryName: props.repositoryName,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       emptyOnDelete: true,
       encryption: ecr.RepositoryEncryption.AES_256
     });
 
-    ecrRepository.addLifecycleRule({ maxImageAge: cdk.Duration.days(7), rulePriority: 1, tagStatus: ecr.TagStatus.UNTAGGED }); // delete images older than 7 days
-    ecrRepository.addLifecycleRule({ maxImageCount: 4, rulePriority: 2, tagStatus: ecr.TagStatus.ANY }); // keep last 4 images
+    this.ecrRepository.addLifecycleRule({ maxImageAge: cdk.Duration.days(7), rulePriority: 1, tagStatus: ecr.TagStatus.UNTAGGED }); // delete images older than 7 days
+    this.ecrRepository.addLifecycleRule({ maxImageCount: 4, rulePriority: 2, tagStatus: ecr.TagStatus.ANY }); // keep last 4 images
 
     const deployImageVersions = props.imageVersion === LATEST_IMAGE_VERSION ? [props.imageVersion] : [props.imageVersion, LATEST_IMAGE_VERSION];
     for (const deployImageVersion of deployImageVersions) {
       // copy from docker registry to ECR
       new ecrDeploy.ECRDeployment(this, `${props.appName}-${props.environment}-${deployImageVersion}-ECRDeployment`, {
         src: new ecrDeploy.DockerImageName('ghcr.io/langfuse/langfuse:latest'),
-        dest: new ecrDeploy.DockerImageName(`${ecrRepository.repositoryUri}:${deployImageVersion}`),
+        dest: new ecrDeploy.DockerImageName(`${this.ecrRepository.repositoryUri}:${deployImageVersion}`),
       });
     }
 
     // print out ecrRepository arn
     new cdk.CfnOutput(this, `${props.appName}-${props.environment}-ECRRepositoryArn`, {
-      value: ecrRepository.repositoryArn,
+      value: this.ecrRepository.repositoryArn,
       exportName: `${props.appName}-${props.environment}-ECRRepositoryArn`,
     });
 
     // print out ecrRepository repository name
     new cdk.CfnOutput(this, `${props.appName}-${props.environment}-ECRRepositoryName`, {
-      value: ecrRepository.repositoryName,
+      value: this.ecrRepository.repositoryName,
       exportName: `${props.appName}-${props.environment}-ECRRepositoryName`,
     });
   }
